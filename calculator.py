1d656ff6b63ee22d6d8d8ea027aad1cd624ad7bb
Feldmarshxll <ivanshvayko2009@gmail.com>
2025-02-18 19:59:20 +0300

    ef

--- calculator.py
+++ calculator.py
@@ -1,49 +1,52 @@
 import math
-one_digit = ['sqrt', 'log', 'log10', 'factorial', 'sin', 'cos', 'tan']
-two_digits = ['+', '-', '/', '*', 'pow', '//', '%', 'log(x, b)']
-not_zero_operations = ['/', '//', '%']
-operation = input('Введите математическую операцию(+, -, /, *, pow, //, %, sqrt, log, log10, log(x, b), factorial, sin, cos, tan): ')
-if operation in one_digit:
-    n = int(input('Введите число(ВВОДИТЬ ТОЛЬКО ЧИСЛО БЕЗ ВСЯКИХ КАВЫЧЕК): '))
-    if operation == 'sqrt':
-        print(f'Корень числа {n} равен {math.sqrt(n)}')
-    elif operation == 'log':
-        print(f'Логарифм числа {n} равен {math.log(n)}')
-    elif operation == 'log10':
-        print(f'Десятичный логарифм числа {n} равен {math.log10(n)}')
-    elif operation == 'factorial':
-        print(f'Факториал числа {n} равен {math.factorial(n)}')
-    elif operation == 'sin':
-        print(f'Синус числа {n} равен {math.sin(n)}')
-    elif operation == 'cos':
-        print(f'Косинус числа {n} равен {math.cos(n)}')
-    elif operation == 'tan':
-        print(f'Тангенс числа {n} равен {math.tan(n)}')
-    else:
-        print('КАК')
-elif operation in two_digits:
-    a = int(input('Введите первое число(ВВОДИТЬ ТОЛЬКО ЧИСЛО БЕЗ ВСЯКИХ КАВЫЧЕК): '))
-    b = int(input('Введите второе число(ВВОДИТЬ ТОЛЬКО ЧИСЛО БЕЗ ВСЯКИХ КАВЫЧЕК): '))
-    if operation in not_zero_operations and b == 0:
-        print('А по попе за деление на ноль?')
-    else:
-        if operation == '+':
-            print(f'Сумма чисел {a} и {b} равна {a + b}')
-        elif operation == '-':
-            print(f'Разность чисел {a} и {b} равна {a - b}')
-        elif operation == '/':
-            print(f'Деление чисел {a} и {b} равна {a / b}')
-        elif operation == '*':
-            print(f'Число {a} умноженное на {b} равно {a * b}')
-        elif operation == 'pow':
-            print(f'Число {a} возведённое в степень {b} равно {math.pow(a, b)}, а число {b} возведённое в степень {a} равно {math.pow(b, a)}')
-        elif operation == '//':
-            print(f'Целочисленное деление числа {a} на число {b} равно {a // b}')
-        elif operation == '%':
-            print(f'Остаток от деления числа {a} на число {b} равно {a % b}')
-        elif operation == 'log(x, b)':
-            print(f'Логарифм числа {a} с основанием {b} равен {math.log(a, b)}')
+try:
+    one_digit = ['sqrt', 'log', 'log10', 'factorial', 'sin', 'cos', 'tan']
+    two_digits = ['+', '-', '/', '*', 'pow', '//', '%', 'log(x, b)']
+    not_zero_operations = ['/', '//', '%']
+    operation = input('Введите математическую операцию(+, -, /, *, pow, //, %, sqrt, log, log10, log(x, b), factorial, sin, cos, tan): ')
+    if operation in one_digit:
+        n = int(input('Введите число(ВВОДИТЬ ТОЛЬКО ЧИСЛО БЕЗ ВСЯКИХ КАВЫЧЕК): '))
+        if operation == 'sqrt':
+            print(f'Корень числа {n} равен {math.sqrt(n)}')
+        elif operation == 'log':
+            print(f'Логарифм числа {n} равен {math.log(n)}')
+        elif operation == 'log10':
+            print(f'Десятичный логарифм числа {n} равен {math.log10(n)}')
+        elif operation == 'factorial':
+            print(f'Факториал числа {n} равен {math.factorial(n)}')
+        elif operation == 'sin':
+            print(f'Синус числа {n} равен {math.sin(n)}')
+        elif operation == 'cos':
+            print(f'Косинус числа {n} равен {math.cos(n)}')
+        elif operation == 'tan':
+            print(f'Тангенс числа {n} равен {math.tan(n)}')
         else:
             print('КАК')
-else:
-    print('ТЫ ЧО ТУПОЙ ЧТО-ЛИ?')
+    elif operation in two_digits:
+        a = int(input('Введите первое число(ВВОДИТЬ ТОЛЬКО ЧИСЛО БЕЗ ВСЯКИХ КАВЫЧЕК): '))
+        b = int(input('Введите второе число(ВВОДИТЬ ТОЛЬКО ЧИСЛО БЕЗ ВСЯКИХ КАВЫЧЕК): '))
+        if operation in not_zero_operations and b == 0:
+            print('А по попе за деление на ноль?')
+        else:
+            if operation == '+':
+                print(f'Сумма чисел {a} и {b} равна {a + b}')
+            elif operation == '-':
+                print(f'Разность чисел {a} и {b} равна {a - b}')
+            elif operation == '/':
+                print(f'Деление чисел {a} и {b} равна {a / b}')
+            elif operation == '*':
+                print(f'Число {a} умноженное на {b} равно {a * b}')
+            elif operation == 'pow':
+                print(f'Число {a} возведённое в степень {b} равно {math.pow(a, b)}, а число {b} возведённое в степень {a} равно {math.pow(b, a)}')
+            elif operation == '//':
+                print(f'Целочисленное деление числа {a} на число {b} равно {a // b}')
+            elif operation == '%':
+                print(f'Остаток от деления числа {a} на число {b} равно {a % b}')
+            elif operation == 'log(x, b)':
+                print(f'Логарифм числа {a} с основанием {b} равен {math.log(a, b)}')
+            else:
+                print('КАК')
+    else:
+        print('ТЫ ЧО ТУПОЙ ЧТО-ЛИ?')
+except:
+    print('uno idioto')
